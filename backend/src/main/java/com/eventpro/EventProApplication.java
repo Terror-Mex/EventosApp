75536fd908af7c71f0cdb5812b41c30351aa9fd0
Adrián Luna <adrianluna012@gmail.com>
2026-03-21 20:13:49 -0600

    fix time zone

--- backend/src/main/java/com/eventpro/EventProApplication.java
+++ backend/src/main/java/com/eventpro/EventProApplication.java
@@ -3,10 +3,17 @@ package com.eventpro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableScheduling
 public class EventProApplication {
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Mexico_City"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(EventProApplication.class, args);
